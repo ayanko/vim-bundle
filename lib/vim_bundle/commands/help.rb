0092288018ce6ed529c0b8c57fdb74784677313b
Andriy Yanko <andriy.yanko@gmail.com>
2010-06-16 09:59:09 +0300

    Implemented zip bundle support

--- lib/vim_bundle/commands/help.rb
+++ lib/vim_bundle/commands/help.rb
@@ -2,9 +2,12 @@ module VimBundle
   module Commands
     def help
       puts <<-HELP
+REQUIRED SYSTEM TOOLS:
+    git, curl, unzip, tar, gz
 USAGE: #{$0} [command]
     init:   install pathogen plugin
     sample: show sample config file
+    update: update all bundles
 HELP
     end
   end
--- lib/vim_bundle/commands/update.rb
+++ lib/vim_bundle/commands/update.rb
@@ -8,48 +8,73 @@ module VimBundle
 
       abort("No bundles defined") if blank_value?(bundles)
         
-      self.erase_bundles
-      self.bundles.each { |bundle| install_bundle(bundle) }
+      self.erase_bundles(bundles)
+      bundles.each { |bundle| install_bundle(bundle) }
     end
 
     protected
 
-    def erase_bundles
+    def erase_bundles(bundles)
+      puts "Erasing all (#{bundles.size}) bundles in #{VimBundle.dir} ..."
+
       FileUtils.rm_rf(VimBundle.dir)
-      File.mkdir(VimBundle.dir)
+      FileUtils.mkdir_p(VimBundle.dir)
     end
 
     def install_bundle(bundle)
-      %w(dir url).each do |key|
-        abort("bundle #{key} is not specified") if self.blank_value?(bundle[key])
-      end
+      validate_bundle(bundle)
 
       puts "Updating bundle #{bundle['dir']} ..."
 
-      case bundle['type']
-      when 'git':
-
-      when 'tgz':
-        dir = File.join(VimBundle.dir, bundle['dir'])
-
-        FileUtils.mkdir_p(dir)
-        system("curl -s #{bundle['url']} | tar xz -C #{dir}") || abort("Failed")
-
-      when 'vim':
-        abort("bundle subdir is not specified") if self.blank_value?(bundle['subdir'])
-        
-        filepath = File.join(VimBundle.dir, bundle['dir'], bundle['subdir'], bundle['dir'] + ".vim")
-
-        FileUtils.mkdir_p(File.dirname(filepath))
-        File.open(filepath, 'w') { |f| f << Net::HTTP.get(URI.parse(bundle['url'])) }
-
-      else
+      method = "install_#{bundle['type']}_bundle"
+    
+      self.respond_to?(method) ?
+        self.send(method, bundle) : 
         abort("Unsupported #{bundle['type'].inspect} file type")
+    end
+
+    def install_git_bundle(bundle)
+      dir = self.create_bundle_dir(bundle)
+      system("git clone -q #{bundle['url']} #{dir}") || abort("Failed")
+      FileUtils.rm_rf(File.join(dir, '.git'))
+    end
+
+    def install_tgz_bundle(bundle)
+      dir = self.create_bundle_dir(bundle)
+      system("curl -s #{bundle['url']} | tar xz -C #{dir}") || abort("Failed")
+    end
+    
+    def install_zip_bundle(bundle)
+      dir = self.create_bundle_dir(bundle)
+      archive_path = File.join(dir, 'bundle.zip')
+      system("curl -s #{bundle['url']} -o #{archive_path}") || abort("Failed")
+      system("unzip -q -d #{dir} #{archive_path}") || abort("Failed")
+      FileUtils.rm_rf(archive_path)
+    end
+
+    def install_vim_bundle(bundle)
+      abort("bundle subdir is not specified") if self.blank_value?(bundle['subdir'])
+
+      filepath = File.join(VimBundle.dir, bundle['dir'], bundle['subdir'], bundle['dir'] + ".vim")
+
+      FileUtils.mkdir_p(File.dirname(filepath))
+      File.open(filepath, 'w') { |f| f << Net::HTTP.get(URI.parse(bundle['url'])) }
+    end
+
+    def validate_bundle(bundle)
+      %w(dir url type).each do |key|
+        abort("bundle #{key} is not specified") if self.blank_value?(bundle[key])
       end
     end
 
     def blank_value?(value)
       value.nil? || value.empty?
+    end
+
+    def create_bundle_dir(bundle)
+      dir = File.join(VimBundle.dir, bundle['dir'])
+      FileUtils.mkdir_p(dir)
+      dir
     end
   end
 end
